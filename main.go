9bd758607383d9c713da6ec34222bd49ba9beecf
ANU8151 <gia.nizharadze@gmail.com>
2025-03-19 21:34:31 +0400

    Refactor account data into struct and improve prompts

--- main.go
+++ main.go
@@ -2,24 +2,36 @@ package main
 
 import "fmt"
 
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
 func main() {
 	login := promptData("Enter Login")
 	password := promptData("Enter Password")
 	url := promptData("Enter URL")
 
-	outputPassword(login, password, url)
+	myAccount := account{
+		login:    login,
+		password: password,
+		url:      url,
+	}
+
+	outputPassword(&myAccount)
 }
 
 func promptData(prompt string) string {
-	fmt.Println(prompt)
+	fmt.Print(prompt + ": ")
 	var res string
 	fmt.Scanln(&res)
 
 	return res
 }
 
-func outputPassword(login, password, url string) {
-	fmt.Println(login)
-	fmt.Println(password)
-	fmt.Println(url)
+func outputPassword(account *account) {
+	fmt.Println(account.login)
+	fmt.Println(account.password)
+	fmt.Println(account.url)
 }
