6fa05f80aa4910d0fff015f4d393843f454b74df
ANU8151 <gia.nizharadze@gmail.com>
2025-03-19 21:15:31 +0400

    Update:Add funcrions promtData & outputPassword

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
+	login := promptData("Enter Login")
+	password := promptData("Enter Password")
+	url := promptData("Enter URL")
 
+	outputPassword(login, password, url)
+}
 
+func promptData(prompt string) string {
+	fmt.Println(prompt)
+	var res string
+	fmt.Scanln(&res)
+
+	return res
+}
+
+func outputPassword(login, password, url string) {
+	fmt.Println(login)
+	fmt.Println(password)
+	fmt.Println(url)
 }
